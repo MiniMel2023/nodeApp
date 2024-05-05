9be79559d650b2d7e663778a25a629ef43723f71
MiniMel2023 <154432597+MiniMel2023@users.noreply.github.com>
2024-05-05 14:12:31 +0200

    Basic Operational before

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -23,18 +24,40 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/cart", {
-      pageTitle: "My Cart",
-      prods: products,
-      path: "/cart",
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => (prod.id = product.id)
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render("shop/cart", {
+        pageTitle: "My Cart",
+        path: "/cart",
+        products: cartProducts,
+      });
     });
   });
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
   res.redirect("/cart");
+};
+
+exports.postCartDeleteItem = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  });
 };
 
 exports.getCheckout = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -10,6 +10,12 @@ router.get("/add-product", adminController.getAddProduct);
 
 router.post("/add-product", adminController.postAddProduct);
 
-router.use("/products", adminController.getProducts);
+router.get("/products", adminController.getProducts);
+
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+router.post("/edit-product", adminController.postEditProduct);
+
+router.post("/delete-product", adminController.postDeleteProduct);
 
 module.exports = router;
